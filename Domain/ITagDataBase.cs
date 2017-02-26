fa293f96053a37187b246ad5804200a05c91c8e5
Felix Fischbach <felixfischbach@gmx.de>
2017-02-26 20:59:19 +0100

    Edited Interface and implemented TagDataBase methods

--- Domain/ITagDataBase.cs
+++ Domain/ITagDataBase.cs
@@ -13,13 +13,14 @@ namespace Domain
         void Connect();                                                 // to start the db or initate a link to it maybe add info here
         void Disconnect();                                              // to close the connection to the db
 
-        void CreateLink(TagData tagData, ArticleData articleData);        // to link TagData to a specific article
-        bool DeleteLink(TagData tagData, ArticleData articleData);      // to delete the set link
+        bool CreateLink(TagData tagData, ArticleData articleData);      // to link TagData to a specific article
+        bool DeleteLink(TagData tagData);                               // to delete the set link by the tag id
+        bool DeleteLinks(ArticleData articleData);                      // to delete the set link by the article id
 
         ArticleData GetArticleDataByTagData(TagData tagData);           // to get the ArticleData linked to the given TagData
         ArticleData GetArticleDataByTagData(int id);                    // to get the ArticleData linked to the given TagData
 
-        TagData GetTagDataByArticleData(ArticleData articleData);       // to get the TagData linked to the given ArticleData
-        TagData GetTagDataByArticleData(int id);                        // to get the TagData linked to the ArticleData with the given Id
+        List<TagData> GetTagDataByArticleData(ArticleData articleData); // to get the TagData linked to the given ArticleData
+        List<TagData> GetTagDataByArticleData(int id);                  // to get the TagData linked to the ArticleData with the given Id
     }
 }
--- Domain/TagDataBase.cs
+++ Domain/TagDataBase.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration.Configuration;
 using System.Data.SqlClient;
 using System.Data.SQLite;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,71 +12,233 @@ namespace Domain
 {
     class TagDataBase : ITagDataBase
     {
+        // Variables
         private const string _dataBaseFileName = "SQLiteTagDataBase.db";
         private SQLiteConnection _connection;
+        private bool _hasOpenedConnection = false;
 
+        // Methods
+
+        /// <summary>
+        /// Sets up a new database file with the link table
+        /// </summary>
         public void SetUpDataBase()
         {
-            // Connect to db
-            Connect();
+            // Check if the database is already connected
+            if (!_hasOpenedConnection)
+            {
+                // Connect to db
+                Connect();
+                _hasOpenedConnection = true;
+            }
 
             // Create command
             SQLiteCommand command = new SQLiteCommand(_connection);
 
             // Specify command
-            // Create table articles
-            command.CommandText = "CREATE TABLE IF NOT EXISTS articles ( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name VARCHAR(100) NOT NULL, note VARCHAR(255), cost VARCHAR(20) );";
-            command.ExecuteNonQuery();
+            // Create table "links"
+            command.CommandText = "CREATE TABLE IF NOT EXISTS links ("
+                                  + "tag_id VARCHAR(30) NOT NULL PRIMARY KEY,"
+                                  + "tag_timestamp VARCHAR(50),"
+                                  + "tag_data VARCHAR(255)"
+                                  + "article_id INTEGER NOT NULL,"
+                                  + "article_name VARCHAR(100) NOT NULL,"
+                                  + "article_note VARCHAR(255),"
+                                  + "article_cost VARCHAR(20) NOT NULL,"
+                                  + ");";
+            /* link
+             * string tag_id (PK)
+             * string tag_timestamp
+             * string tag_data
+             * int article_id
+             * string article_name
+             * string article_note
+             * string article_cost
+             */
 
-            // Create table tags
-            command.CommandText = "CREATE TABLE IF NOT EXISTS tags ( id VARCHAR(255) NOT NULL PRIMARY KEY, timestamp VARCHAR(30) NOT NULL, data VARCHAR(255) );";
+            // Execute query
             command.ExecuteNonQuery();
-
+            
             // Freeing the resources
             command.Dispose();
         }
 
         public void Connect()
         {
+            // Check if the connection is already opened
+            if (_hasOpenedConnection)
+                return;
+
+            // Set up connection
             _connection = new SQLiteConnection();
             _connection.ConnectionString = "Data Source=" + _dataBaseFileName;
+
+            // Open connection
             _connection.Open();
+            _hasOpenedConnection = true;
         }
 
         public void Disconnect()
         {
+            // Check if the connection is openend
+            if (_hasOpenedConnection == false)
+                return;
+
+            // Close connection
             _connection.Close();
+            _hasOpenedConnection = false;
+
+            // Free resources
             _connection.Dispose();
         }
 
-        public void CreateLink(TagData tagData, ArticleData articleData)
+        public bool CreateLink(TagData tagData, ArticleData articleData)
         {
-            throw new NotImplementedException();
+            // Check if a conenction is open
+            if (!_hasOpenedConnection)
+                return false;
+
+            // Create query
+            string query =
+                "INSERT INTO links (tag_id, tag_timestamp, tag_data, article_id, article_name, article_note, article_cost) VALUES ("
+                + tagData.Id + ", " + tagData.TimeStamp.ToString("dd.MM.yyyy HH:mm:ss") + ", " + tagData.Data + ", "
+                + articleData.Id + ", " + articleData.Name + ", " + articleData.Note + ", " + articleData.Cost + ");";
+
+            return CreateAndExecuteCommand(query);
         }
 
-        public bool DeleteLink(TagData tagData, ArticleData articleData)
+        public bool DeleteLink(TagData tagData)
         {
-            throw new NotImplementedException();
+            if (!_hasOpenedConnection)
+                return false;
+
+            // Create query
+            string query = $"DELETE FROM links WHERE tag_id={tagData.Id}";
+
+            // Create and execute query
+            return CreateAndExecuteCommand(query);
+        }
+
+        public bool DeleteLinks(ArticleData articleData)
+        {
+            if (!_hasOpenedConnection)
+                return false;
+
+            // Create query
+            string query = $"DELETE FROM links WHERE article_id={articleData.Id}";
+
+            // Create and execute query
+            return CreateAndExecuteCommand(query);
         }
 
         public ArticleData GetArticleDataByTagData(TagData tagData)
         {
-            throw new NotImplementedException();
+            return GetArticleDataByTagData(Convert.ToInt32(tagData.Id));
         }
 
         public ArticleData GetArticleDataByTagData(int id)
         {
-            throw new NotImplementedException();
+            // Check if the connection is open
+            if (!_hasOpenedConnection)
+                return null;
+
+            // Create query
+            string query = $"SELECT article_id, article_name, article_note, article_cost FROM links WHERE tag_id={id}";
+
+            // Create command
+            SQLiteCommand command = new SQLiteCommand(query, _connection);
+
+            // Create reader
+            SQLiteDataReader reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+                int resultId = 0;
+                decimal resultCost = 0.00M;
+                ArticleData result = new ArticleData();
+
+                // Try parse id
+                if (!Int32.TryParse(reader[0].ToString(), out resultId))
+                {
+                    // return if not successful
+                    return null;
+                }
+
+                // Try parse cost
+                if (!Decimal.TryParse(reader[3].ToString(), out resultCost))
+                {
+                    // return null if not successful
+                    return null;
+                }
+
+                // Save results in obect
+                result.Id = resultId;
+                result.Name = reader[1].ToString();
+                result.Note = reader[2].ToString();
+                result.Cost = resultCost;
+
+                return result;
+            }
+
+            return null;
         }
 
-        public TagData GetTagDataByArticleData(ArticleData articleData)
+        public List<TagData> GetTagDataByArticleData(ArticleData articleData)
         {
-            throw new NotImplementedException();
+            return GetTagDataByArticleData(articleData.Id);
         }
 
-        public TagData GetTagDataByArticleData(int id)
+        public List<TagData> GetTagDataByArticleData(int id)
         {
-            throw new NotImplementedException();
+            List<TagData> resultList = new List<TagData>();
+
+            /* link
+             * string tag_id (PK)
+             * string tag_timestamp
+             * string tag_data
+             * int article_id
+             * string article_name
+             * string article_note
+             * string article_cost
+             */
+
+            // Check if the connection is open
+            if (!_hasOpenedConnection)
+                return null;
+
+            // Create query
+            string query = $"SELECT tag_id, tag_timestamp, tag_data, FROM links WHERE article_id={id}";
+
+            // Create command
+            SQLiteCommand command = new SQLiteCommand(query, _connection);
+
+            // Create reader
+            SQLiteDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                // Add the result to the list
+                resultList.Add(new TagData(
+                    reader[0].ToString(),
+                    DateTime.ParseExact(reader[1].ToString(), "dd.MM.yyyy HH:mm:ss", CultureInfo.InvariantCulture),
+                    reader[2].ToString()));
+            }
+
+            return resultList;
+        }
+
+        private bool CreateAndExecuteCommand(string query)
+        {
+            if (!_hasOpenedConnection)
+                return false;
+
+            // Create command
+            SQLiteCommand command = new SQLiteCommand(query, _connection);
+
+            // Execute query
+            command.ExecuteNonQuery();
+
+            return true;
         }
     }
 }
