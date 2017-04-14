9bf5d7fe2fac08864393bc5a825be0c332136625
Felix Fischbach <felixfischbach@gmx.de>
2017-04-14 14:35:31 +0200

    Implement Configuration Window

--- ConfigurationWindow/MainWindow.xaml.cs
+++ ConfigurationWindow/MainWindow.xaml.cs
@@ -48,17 +48,127 @@ namespace ConfigurationWindow
 
         private void buttonSearch_Click(object sender, RoutedEventArgs e)
         {
-            // TODO Search for ArticleName, ArticleNumber and ChipNumber
+            bool tagsFound = false;
+            bool articleFound = false;
+
+            // Check for db conntection
+            if (!tagDb.IsConnected())
+                return;
+
+            // Clear previous search entries
+            SearchEntries.Clear();
+
+            // Check which fields are set
+            if (textBoxSearch.Text != "")
+            {
+                int parsedNumber = 0;
+
+                // Try parse given number
+                if (!Int32.TryParse(textBoxSearch.Text, out parsedNumber))
+                {
+                    labelStatus.Content = "Status: Bitte gültige Nummer eingeben!";
+                    return;
+                }
+
+                // Search by chip number
+                ArticleData articleFromDb = tagDb.GetArticleDataByTagData(parsedNumber);
+
+                // Check return value
+                if (articleFromDb != null)
+                {
+                    articleFound = true;
+
+                    SearchEntries.Add(new SearchEntry(articleFromDb.Id.ToString(), articleFromDb.Name, articleFromDb.Note, articleFromDb.Cost,
+                        "", DateTime.Now, ""));
+
+                    // TODO Refresh search entries? needed?
+                }
+
+                // Search by article number
+                List<TagData> tagsFromDb = tagDb.GetTagDataByArticleData(parsedNumber);
+
+                // Check return value
+                if (tagsFromDb.Count >= 1)
+                {
+                    tagsFound = true;
+
+                    foreach (var td in tagsFromDb)
+                    {
+                        SearchEntries.Add(new SearchEntry("", "", "", 0.00M, td.Id, td.TimeStamp, td.Data));
+                    }
+
+                    // TODO Refresh search entries? needed?
+                }
+
+                // Set label
+                if (articleFound)
+                {
+                    if (tagsFound)
+                    {
+                        labelStatus.Content = "Status: Artikel und Tags gefunden.";
+                    }
+                    else
+                    {
+                        labelStatus.Content = "Status: Artikel gefunden.";
+                    }
+                }
+                else
+                {
+                    if (tagsFound)
+                    {
+                        labelStatus.Content = "Status: Tags gefunden.";
+                    }
+                    else
+                    {
+                        labelStatus.Content = "Status: Suche ergab keine Ergebnisse.";
+                    }
+                }
+
+            }
         }
 
         private void buttonDeleteLink_Click(object sender, RoutedEventArgs e)
         {
-            // TODO Delete link fromdatabase
+            // Check for db connection
+            if (!tagDb.IsConnected())
+                return;
+
+            // Check if field is set
+            if (textBoxChipNumber.Text != "")
+            {
+                // Delete the entry with the given chip number
+                if (tagDb.DeleteLink(new TagData(textBoxChipNumber.Text, DateTime.Now, "")))
+                {
+                    // Tag could be deleted
+                    labelStatus.Content = "Status: Link erfolgreich gelöscht.";
+                }
+                else
+                {
+                    labelStatus.Content = "Status: Link konnte nicht gelöscht werden";
+                }
+            }
         }
 
         private void buttonAddLink_Click(object sender, RoutedEventArgs e)
         {
-            // TODO Add link to database
+            TagData tagData = new TagData(textBoxChipNumber.Text, DateTime.Parse(textBoxChipTimestamp.Text), textBoxChipData.Text);
+            ArticleData articleData = new ArticleData();
+            articleData.Id = Convert.ToInt32(textBoxArticleNumber.Text);
+            articleData.Name = textBoxArticleName.Text;
+            articleData.Cost = Convert.ToDecimal(textBoxArticlePrice.Text);
+            articleData.Note = textBoxArticleNote.Text;
+
+            if (tagDb.IsConnected())
+            {
+                if (tagDb.CreateLink(tagData, articleData))
+                {
+                    labelStatus.Content = "Status: Link erfolgreich hinzugefügt.";
+                }
+                else
+                {
+                    labelStatus.Content = "Status: Link konnte nicht hinzugefügt werden!";
+                }
+            }
         }
     }
 
@@ -73,7 +183,7 @@ namespace ConfigurationWindow
         public string TagId;
 
 
-        private SearchEntry(string articleNumber, string articleName, string articleNote, decimal articleCost,
+        public SearchEntry(string articleNumber, string articleName, string articleNote, decimal articleCost,
             string tagId, DateTime tagTimestamp, string tagData)
         {
             ArticleNumber = articleNumber;
